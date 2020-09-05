c60e194f66d226563a85bdabfb58bbc59437957d
LeMicheluti <lemicheluti@gmail.com>
2020-09-05 11:15:25 -0300

    Refactor entities

--- PaymentContext/PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Subscription.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdate { get; set; }
-        public DateTime? ExpireDate { get; set; }
-        public bool Active { get; set; }
-        public List<Payment> Payments { get; set; }
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
+        {
+            CreateDate = DateTime.Now;
+            LastUpdate = DateTime.Now;
+            ExpireDate = expireDate;
+            Active = true;
+            _payments = new List<Payment>();
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get => _payments.ToArray(); }
+
+        public void AddPayment(Payment payment)
+        {
+            _payments.Add(payment);
+        }
+
+        public void Activate()
+        {
+            Active = true;
+            LastUpdate = DateTime.Now;
+        }
+
+        public void Inactivate()
+        {
+            Active = false;
+            LastUpdate = DateTime.Now;
+        }
     }
 }
