ebbdc7dbce47529203e8c0d74f5410fbd1332f34
LeMicheluti <lemicheluti@gmail.com>
2020-09-05 11:42:53 -0300

    Add Value Objects

---
+++ PaymentContext/PaymentContext.Domain/ValueObjects/Name.cs
@@ -0,0 +1,16 @@
+﻿using PaymentContext.Shared.ValueObjects;
+
+namespace PaymentContext.Domain.ValueObjects
+{
+    public class Name : ValueObject
+    {
+        public Name(string fisrtName, string lastName)
+        {
+            FisrtName = fisrtName;
+            LastName = lastName;
+        }
+
+        public string FisrtName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
