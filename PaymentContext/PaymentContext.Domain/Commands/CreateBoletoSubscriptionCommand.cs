2972507897ebc3b823324d7b4ef965e7a3943f6c
LeMicheluti <lemicheluti@gmail.com>
2020-09-10 20:56:45 -0300

    Fail Fast Validation and command test

--- PaymentContext/PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext/PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,9 +1,12 @@
-﻿using PaymentContext.Domain.Enums;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 using System;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
         public string FisrtName { get; set; }
         public string LastName { get; set; }
@@ -29,5 +32,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FisrtName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FisrtName, 40, "Name.FisrtName", "Nome deve conter até 40 caracteres")
+            );
+        }
     }
 }
---
+++ PaymentContext/PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErroWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand
+            {
+                FisrtName = ""
+            };
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
