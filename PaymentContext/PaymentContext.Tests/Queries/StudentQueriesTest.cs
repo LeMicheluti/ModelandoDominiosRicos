9ea3053689e4a2132e7b0f180f9636dbd9506980
LeMicheluti <lemicheluti@gmail.com>
2020-09-11 07:47:14 -0300

    Queries and Tests

---
+++ PaymentContext/PaymentContext.Tests/Queries/StudentQueriesTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTest
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTest()
+        {
+            _students = new List<Student>();
+            for (int i = 0; i < 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("1111111111" + i.ToString(), EDocumentType.CPF),
+                    new Email(i.ToString() + "@gmail.com")
+                ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnErroWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("12345678911");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, student);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErroWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("11111111111");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreNotEqual(null, student);
+        }
+    }
+}
