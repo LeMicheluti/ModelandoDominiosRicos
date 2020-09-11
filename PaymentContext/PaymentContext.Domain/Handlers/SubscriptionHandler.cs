41fc3db0c9d16e4de1d24a36b2b27daf729422c3
LeMicheluti <lemicheluti@gmail.com>
2020-09-10 21:33:58 -0300

    Repository and Handlers

---
+++ PaymentContext/PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -0,0 +1,125 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Repositories;
+using PaymentContext.Domain.Services;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Commands;
+using PaymentContext.Shared.Handlers;
+using System;
+
+namespace PaymentContext.Domain.Handlers
+{
+    public class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>, IHandler<CreatePayPalSubscriptionCommand>
+    {
+        private readonly IStudentRepository _repository;
+        private readonly IEmailService _emailService;
+
+        public SubscriptionHandler(IStudentRepository repository, IEmailService emailService)
+        {
+            _repository = repository;
+            _emailService = emailService;
+        }
+
+        public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
+        {
+            // Fail Fast Validations
+            command.Validate();
+            if (command.Invalid)
+            {
+                AddNotifications(command);
+                return new CommandResult(false, "Não foi possível realizar sua assinatura");
+            }
+
+            // Verificar se Documento já esta cadastrado
+            if (_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
+            // Verificar se E-mail já está cadastrado
+            if (_repository.EmailExists(command.Document))
+                AddNotification("Email", "Este E-mail já está em uso");
+
+            // Gerar os VOs
+            var name = new Name(command.FisrtName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighbothood, command.City, command.State, command.Country, command.ZipCode);
+            
+            // Gerar as entidades
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new BoletoPayment(command.BarCode,
+                                            command.BoletoNumber,
+                                            command.PaidDate,
+                                            command.ExpireDate,
+                                            command.Total,
+                                            command.TotalPaid,
+                                            command.Payer,
+                                            new Document(command.PayerDocument, command.PayerDocumentType),
+                                            address,
+                                            email);
+
+            // Relazionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Agrupar as validações
+            AddNotifications(name, document, email, address, student, subscription, payment);
+
+            // Salvar as Informações
+            _repository.CreateSubsctiption(student);
+
+            // Enviar E-mail de boas vindas
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "Bem vindo", "Sua assinatura foi criada");
+
+            return new CommandResult(true, "Assinatura reliazada com sucesso");
+        }
+
+        public ICommandResult Handle(CreatePayPalSubscriptionCommand command)
+        {
+            // Verificar se Documento já esta cadastrado
+            if (_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
+            // Verificar se E-mail já está cadastrado
+            if (_repository.EmailExists(command.Document))
+                AddNotification("Email", "Este E-mail já está em uso");
+
+            // Gerar os VOs
+            var name = new Name(command.FisrtName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighbothood, command.City, command.State, command.Country, command.ZipCode);
+
+            // Gerar as entidades
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new PayPalPayment(command.TransactionCode,
+                                            command.PaidDate,
+                                            command.ExpireDate,
+                                            command.Total,
+                                            command.TotalPaid,
+                                            command.Payer,
+                                            new Document(command.PayerDocument, command.PayerDocumentType),
+                                            address,
+                                            email);
+
+            // Relazionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Agrupar as validações
+            AddNotifications(name, document, email, address, student, subscription, payment);
+
+            // Salvar as Informações
+            _repository.CreateSubsctiption(student);
+
+            // Enviar E-mail de boas vindas
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "Bem vindo", "Sua assinatura foi criada");
+
+            return new CommandResult(true, "Assinatura reliazada com sucesso");
+        }
+    }
+}
--- PaymentContext/PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext/PaymentContext.Domain/ValueObjects/Name.cs
@@ -7,18 +7,23 @@ namespace PaymentContext.Domain.ValueObjects
     {
         public Name(string fisrtName, string lastName)
         {
-            FisrtName = fisrtName;
+            FirstName = fisrtName;
             LastName = lastName;
 
             AddNotifications(new Contract()
                 .Requires()
-                .HasMinLen(FisrtName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
                 .HasMinLen(LastName, 3, "Name.LastName", "Nome deve conter pelo menos 3 caracteres")
-                .HasMaxLen(FisrtName, 40, "Name.FisrtName", "Nome deve conter até 40 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FisrtName", "Nome deve conter até 40 caracteres")
             );
         }
 
-        public string FisrtName { get; private set; }
+        public string FirstName { get; private set; }
         public string LastName { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
     }
 }
